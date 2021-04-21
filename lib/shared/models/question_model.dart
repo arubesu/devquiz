70420d0a84bab9064279d07801c1986987731ab0
Bruno Souza <bruno.arubesu@gmail.com>
2021-04-21 14:09:31 -0300

    add data helpers to models

--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+
 import 'answer_model.dart';
 
 class QuestionModel {
@@ -10,4 +14,37 @@ class QuestionModel {
   }) : assert(
           answers.length == 4,
         );
+
+  QuestionModel copyWith({
+    String? title,
+    List<AnswerModel>? answers,
+  }) {
+    return QuestionModel(
+      title: title ?? this.title,
+      answers: answers ?? this.answers,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'answers': answers.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory QuestionModel.fromMap(Map<String, dynamic> map) {
+    return QuestionModel(
+      title: map['title'],
+      answers: List<AnswerModel>.from(
+          map['answers'].map((x) => AnswerModel.fromMap(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuestionModel.fromJson(String source) =>
+      QuestionModel.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'QuestionModel(title: $title, answers: $answers)';
 }
--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:DevQuiz/shared/helpers/helpers.dart';
+import 'package:flutter/foundation.dart';
+
 import 'question_model.dart';
 
 enum Level {
@@ -11,14 +15,61 @@ class QuizModel {
   final String title;
   final List<QuestionModel> questions;
   final int questionsAnswered;
-  final String imageSrc;
+  final String image;
   final Level level;
 
   QuizModel({
     required this.title,
     required this.questions,
     this.questionsAnswered = 0,
-    required this.imageSrc,
+    required this.image,
     required this.level,
   });
+
+  QuizModel copyWith({
+    String? title,
+    List<QuestionModel>? questions,
+    int? questionsAnswered,
+    String? image,
+    Level? level,
+  }) {
+    return QuizModel(
+      title: title ?? this.title,
+      questions: questions ?? this.questions,
+      questionsAnswered: questionsAnswered ?? this.questionsAnswered,
+      image: image ?? this.image,
+      level: level ?? this.level,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'questions': questions.map((x) => x.toMap()).toList(),
+      'questionsAnswered': questionsAnswered,
+      'image': image,
+      'level': describeEnum(level),
+    };
+  }
+
+  factory QuizModel.fromMap(Map<String, dynamic> map) {
+    return QuizModel(
+      title: map['title'],
+      questions: List<QuestionModel>.from(
+          map['questions']?.map((x) => QuestionModel.fromMap(x))),
+      questionsAnswered: map['questionsAnswered'],
+      image: map['image'],
+      level: enumFromString(Level.values, map['level']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuizModel.fromJson(String source) =>
+      QuizModel.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'QuizModel(title: $title, questions: $questions, questionsAnswered: $questionsAnswered, image: $image, level: $level)';
+  }
 }
