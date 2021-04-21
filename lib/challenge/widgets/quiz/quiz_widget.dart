578431381565116e1478aa472b8d5b38dd52ceca
Bruno Souza <bruno.arubesu@gmail.com>
2021-04-20 23:06:11 -0300

    feat: add QuizWidget

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,26 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+
+  const QuizWidget({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Text(
+          title,
+          style: AppTextStyles.heading,
+        ),
+        SizedBox(
+          height: 24,
+        ),
+      ],
+    ));
+  }
+}
